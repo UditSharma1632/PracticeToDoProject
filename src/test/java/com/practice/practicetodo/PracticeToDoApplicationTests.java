0f175d3ef9844bbfa315cbc0af6418e9896fd53d
Udit Sharma <uditsharma1632@gmail.com>
2023-07-16 15:37:44 +0530

    first commit

---
+++ src/test/java/com/practice/practicetodo/PracticeToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.practicetodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
